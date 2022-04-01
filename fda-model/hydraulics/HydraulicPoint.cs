dd429592f313ffe451cb6bccf99df666ec90949d
bbeam <Brennan.B.Beam@usace.army.mil>
2022-04-01 10:13:54 -0700

    create oject to store WSE elevations

---
+++ fda-model/hydraulics/HydraulicPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fda_model.hydraulics
+{
+    public class HydraulicPoint
+{
+        double x;
+        double y;
+        List<Tuple<double, double>> probsValues;
+
+        public HydraulicPoint()
+        {
+
+        }
+}
+}
