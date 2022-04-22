e22fbb37f1ca65fe072315dc6a7e37540d180441
Will Lehman <william.p.lehman@usace.army.mil>
2022-04-22 09:33:00 -0400

    adding structure and occupancy type

---
+++ fda-model/structures/OccupancyType.cs
@@ -0,0 +1,16 @@
+﻿namespace structures
+{
+    public class OccupancyType
+    {
+        private string name;
+        private string damcat;
+        private paireddata.UncertainPairedData _StructureDamageFunction;
+        private paireddata.UncertainPairedData _ContentDamageFunction;
+        private paireddata.UncertainPairedData _OtherDamageFunction;
+        //other stuff.
+        public paireddata.UncertainPairedData StructureDamageFunction
+        {
+            get { return _StructureDamageFunction; }
+        }
+    }
+}
---
+++ fda-model/structures/Structure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structures
+{
+    public class Structure
+    {
+        private string name;
+        private double _x;
+        private double _y;
+        private Statistics.ContinuousDistribution _foundationHeight;
+        private Statistics.ContinuousDistribution _StructureValue;
+        private Statistics.ContinuousDistribution _ContentValue;
+        private Statistics.ContinuousDistribution _OtherValue;
+        private OccupancyType _occtype;
+        public double ComputeDamage(double depth)
+        { 
+            //sample stuff... (should probably be in a method to return a deterministic structure
+            double foundHeightSample = _foundationHeight.InverseCDF(.5);
+            double structValueSample = _StructureValue.InverseCDF(.5);
+            paireddata.IPairedData structDamagePairedData = _occtype.StructureDamageFunction.SamplePairedData(.5);
+            
+            double depthabovefoundHeight = depth - foundHeightSample;
+            double structDamagepercent = structDamagePairedData.f(depthabovefoundHeight);
+
+            return structDamagepercent*structValueSample;
+        }
+    }
+}
+
