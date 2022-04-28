4ebdd3974a40c7ad0be25fff521f5621e19e153e
bbeam <Brennan.B.Beam@usace.army.mil>
2022-04-28 15:34:39 -0700

    using RASMapper to load a structure inventory full of the attributes from a shapefile. Will need to make this work for database too.

--- fda-model/structures/Inventory.cs
+++ fda-model/structures/Inventory.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using RasMapperLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace structures
 {
@@ -10,6 +12,45 @@ namespace structures
 {
         private List<Structure> _structures;
         private OccupancyTypeSet _Occtypes;
+
+        //These guys are all the headers from the NSI I want to save in our structure objects
+        //These specifically line up with the parameters for the structure constructor
+        private String[] _columnsOfInterest = new String[] {"fd_id","x","y","found_ht","val_struct",
+            "val_cont","val_vehic","st_damcat","occtype","pop2amu65","pop2amo65","pop2pmu65","pop2pm065"};
+        
+        public Inventory(string pointShapefilePath)
+        {
+            PointFeatureLayer structureInventory = new PointFeatureLayer("Structure_Inventory", pointShapefilePath);
+            List<string> names = (List<string>)structureInventory.GetValuesFromColumn("fd_id").OfType<string>();
+            List<double> x = (List<double>) structureInventory.GetValuesFromColumn("x").OfType<double>();
+            List<double> y = (List<double>) structureInventory.GetValuesFromColumn("y").OfType<double>();
+            List<double> foundheight = (List<double>) structureInventory.GetValuesFromColumn("found_ht").OfType<double>();
+            List<double> valStructure = (List<double>) structureInventory.GetValuesFromColumn("val_cont").OfType<double>();
+            List<double> valContent = (List<double>) structureInventory.GetValuesFromColumn("val_cont").OfType<double>();
+            List<double> valVehic = (List<double>) structureInventory.GetValuesFromColumn("val_vehic").OfType<double>();
+            List<string> damCat = (List<string>) structureInventory.GetValuesFromColumn("st_damcat").OfType<string>();
+            List<string> occtype = (List<string>) structureInventory.GetValuesFromColumn("occtype").OfType<string>();
+            List<int>    pop2amu65 = (List<int>) structureInventory.GetValuesFromColumn("pop2amu65").OfType<int>();
+            List<int>    pop2amo65 = (List<int>) structureInventory.GetValuesFromColumn("pop2amo65").OfType<int>();
+            List<int>    pop2pmu65 = (List<int>) structureInventory.GetValuesFromColumn("pop2pmu65").OfType<int>();
+            List<int>    pop2pmo65 = (List<int>) structureInventory.GetValuesFromColumn("pop2pmo65").OfType<int>();
+            _structures = new List<Structure>();
+
+            for(int i = 0; i < names.Count(); i++)
+            {
+                _structures.Add(new Structure(names[i], x[i], y[i], foundheight[i], valStructure[i], valContent[i], valVehic[i], damCat[i], occtype[i], pop2amu65[i], pop2amo65[i], pop2pmu65[i], pop2pmo65[i]));
+            }
+
+            IEnumerable<Point> sIPoints = structureInventory.Points();
+            foreach(string header in _columnsOfInterest)
+            {
+
+            }
+            foreach (Point point in sIPoints)
+            {
+                //This is gonna rely on the NSI column headers. 
+            }
+        }
         public DeterministicInventory Sample(int seed)
         {
             Random random = new Random(seed);
--- fda-model/structures/Structure.cs
+++ fda-model/structures/Structure.cs
@@ -8,25 +8,50 @@ namespace structures
 {
     public class Structure
     {
-        private string name;
+        private string _name;
         private double _x;
         private double _y;
         private double _foundationHeightMean;
-        private Statistics.ContinuousDistribution _StructureValue;
+        private Statistics.ContinuousDistribution _StructureValue; //ehhh, how do we get these distributions? we don't define them at the structure level. 
         private Statistics.ContinuousDistribution _ContentValue;
         private Statistics.ContinuousDistribution _OtherValue;
-        private string occtype_name;
-        private string damcat_name;
+        private double _StructureValueFromInput;
+        private double _ContentValueFromInput;
+        private double _OtherValueFromInput;
+        private string _occtype_name;
+        private string _damcat_name;
+        private int _pop2amu65;
+        private int _pop2amo65;
+        private int _pop2pmu65;
+        private int _pop2pmo65;
 
-        public string DamCatName { get { return damcat_name; } }
-        public string OccTypeName { get { return occtype_name; } }
+        //This parameter list lines up with columnsOfInterest in the Inventory 
+        public Structure(string name, double x, double y, double foundationHeightMean,double structureValue, double contentValue, 
+            double vehicleValue, string damCat, string occtype, int pop2amu65, int pop2amo65, int pop2pmu65, int pop2pm065)
+        {
+            _name = name;
+            _x = x;
+            _y = y;
+            _foundationHeightMean = foundationHeightMean;
+            _StructureValueFromInput = structureValue;
+            _ContentValueFromInput = contentValue;
+            _OtherValueFromInput = vehicleValue;
+            _occtype_name = occtype;
+            _damcat_name = damCat;
+            _pop2amu65 = pop2amu65;
+            _pop2amo65 = pop2amo65;
+            _pop2pmu65 = pop2pmu65;
+            _pop2pmo65 = pop2pm065;
+        }
+        public string DamCatName { get { return _damcat_name; } }
+        public string OccTypeName { get { return _occtype_name; } }
         public DeterministicStructure Sample(int seed, DeterministicOccupancyType occtype)
         {
             Random random = new Random(seed);
             double foundHeightSample = _foundationHeightMean + (_foundationHeightMean * occtype.FoundationHeightError);
             double structValueSample = _StructureValue.InverseCDF(random.NextDouble());
             //load up the deterministic structure
-            return new DeterministicStructure(name,structValueSample,foundHeightSample);
+            return new DeterministicStructure(_name,structValueSample,foundHeightSample);
         }
 
     }
