dec7dadaa07a3f4d6c9f9d08acf47445208b26b7
bbeam <Brennan.B.Beam@usace.army.mil>
2022-04-29 13:58:54 -0700

    temporary tests which should not run.  Just for testing rasmapperlib and learning. rely on outside data and files.

---
+++ fda-model-test/unittests/InventoryShould.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using structures;
+using System.IO;
+using System;
+using Geospatial.GDALAssist;
+
+namespace fda_model_test.unittests
+{
+    public class InventoryShould
+    {
+        static InventoryShould()
+        {
+            string gdalPath = "C:\\Programs\\6.x Development\\GDAL\\";
+            if (!Directory.Exists(gdalPath))
+            {
+                Console.WriteLine("GDAL directory not found: " + gdalPath);
+                return;
+            }
+            GDALSetup.InitializeMultiplatform(gdalPath);
+        }
+        [Fact]
+        public void loadAStructureInventoryFromShapefile()
+        {
+            var SI = new Inventory("C:\\TestData\\NSI\\StructureInventory.shp");
+
+        }
+    }
+}
