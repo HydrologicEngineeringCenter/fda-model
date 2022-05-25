51e74ba3eca70331f80dddb1a3a93e97f5bd3034
bbeam <Brennan.B.Beam@usace.army.mil>
2022-05-25 13:48:08 -0700

    damage compute simplifications

--- fda-model/compute/StageAggregatedDamageCompute.cs
+++ fda-model/compute/StageAggregatedDamageCompute.cs
@@ -1,10 +1,10 @@
 ﻿using structures;
 using System.Collections.Generic;
-using System.Linq;
 using RasMapperLib;
 using fda_hydro.hydraulics;
 using paireddata;
 using System;
+using fda_model.metrics;
 
 namespace fda_model.compute
 {
@@ -22,41 +22,28 @@ namespace fda_model.compute
         private void Compute(int seed, int iterations)
         {
             Random randomNumberGenerator = new Random(seed);
-
             //get the XYs off the inventory 
             PointMs structurePoints = _inventory.GetPointMs();
+            StageDamageOrdinate stageDamageOrdinate = new StageDamageOrdinate();
             //Create StageDamageOrdinate 
             //Create IList of DamageResult
 
             //iterate across hydraulics datasets -- we want at each dataset - for a given impact area, for a given damage catagory,
             //for a given asset catagory, we need a distribution of damages
-            foreach(HydraulicProfile set in _hydraulics.HydraulicProfiles)
+            foreach (HydraulicProfile set in _hydraulics.HydraulicProfiles)
             {
                 //get depths for all structure in the inventory 
-                float[] depths = set.GetHydraulicDataFromUnsteadyHDFs(structurePoints);
+                float[] depths = set.GetDepths(structurePoints);
 
                 //add uncertainty 
                 for (int i = 0; i < iterations; i++)
                 {
-                    DeterministicInventory sampledInventory = _inventory.Sample(randomNumberGenerator.Next());
-
+                    DeterministicInventory sampledInventory = _inventory.Sample(randomNumberGenerator.Next()); //resampling the variable properties on those structures 
                     //compute an iteration of damage for all structures for this hydraulic set
-                    for (int j = 0; j < depths.Count(); j++)
-                    {
-                        if (depths[j] != -9999)
-                        {
-                            StructureDamageResult results = sampledInventory.Inventory[j].ComputeDamage(depths[j]);
-                            //add result to proper damage result
-                        }
-                        //this is the loop where we want to load up the histogram with damages by catagory, impact area
-                    }
+                    StructureDamageResult results = sampledInventory.ComputeDamages(depths);
                 }
             }
         }
-        private PairedData GetStageAggDamCurves(IList<IList<StructureDamageResult>> structureDamageResults, PairedData indexPoint)
-        {
-            return null;
-            //TODO: figure out how to aggregate to this bad boy
-        }
+
     }
 }
