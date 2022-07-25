d126fd68eee74324be2c42125e6febc9d519a3f2
Brennan Beam <BrennanBeam@gmail.com>
2022-07-25 13:42:06 -0700

    fixing references

---
+++ fda-model-test/test/HydraulicProfileShould.cs
@@ -0,0 +1,65 @@
+using Xunit;
+using fda_hydro.hydraulics;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using Geospatial.GDALAssist;
+
+namespace fda_model_test_unittests
+{
+    /// <summary>
+    ///Test data needs to all be in the same projection. This test requirest the quick start guide dataset be saved in the locations in the strings. This test should not be run remotely, and can eventually be deleted. It exists to help test and validate the design
+    /// of the aggregated stage damage compute.
+    /// </summary>
+    public class HydraulicProfileShould
+    {
+
+        static string terrainFile = "C:\\TestData\\Terrain\\Terrain.hdf";
+        static string SIFile = "C:\\TestData\\NSI\\StructureInventory.shp";
+        static string impactAreaFile = "C:\\TestData\\ImpactAreas\\ImpactAreas.shp";
+
+        static string[] resultsFiles = new string[]
+         {
+                "C:\\TestData\\Native Output Files\\Muncie.p08.hdf",
+                "C:\\TestData\\Native Output Files\\Muncie.p07.hdf",
+                "C:\\TestData\\Native Output Files\\Muncie.p06.hdf",
+                "C:\\TestData\\Native Output Files\\Muncie.p05.hdf",
+                "C:\\TestData\\Native Output Files\\Muncie.p04.hdf",
+                "C:\\TestData\\Native Output Files\\Muncie.p03.hdf",
+                "C:\\TestData\\Native Output Files\\Muncie.p02.hdf",
+                "C:\\TestData\\Native Output Files\\Muncie.p01.hdf" 
+        };
+
+        static double[] probabilities = new double[] { .002, .005, .01, .05, .1, .2, .5, .9 };
+
+
+        static HydraulicProfileShould()
+        {
+            string gdalPath = @"C:\Programs\GDAL\GDAL\";
+            if (!Directory.Exists(gdalPath))
+            {
+                Console.WriteLine("GDAL directory not found: " + gdalPath);
+                return;
+            }
+            GDALSetup.InitializeMultiplatform(gdalPath);
+        }
+
+        [Fact]
+        public void sampleSomeValues()
+        {
+            List<HydraulicProfile> profiles = new List<HydraulicProfile>();
+            for(int i = 0; i < resultsFiles.Length; i++)
+            {
+             profiles.Add(new HydraulicProfile(probabilities[i],resultsFiles[i],terrainFile, fda_model.hydraulics.enums.HydraulicDataSource.UnsteadyHDF));
+            }
+            HydraulicDataset dataset = new HydraulicDataset(profiles);
+            Inventory inventory = new Inventory(SIFile,impactAreaFile);
+            float[] depths = dataset.HydraulicProfiles[0].GetDepths(inventory.GetPointMs());
+            Console.WriteLine("we did it!");
+        }
+    }
+
+}
+
+
+
